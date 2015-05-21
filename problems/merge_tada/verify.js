ab765a2dba287b7d6964764aa68d297e7ca81a20
Taylor Blau <me@ttaylorr.com>
2015-05-20 20:30:45 -0400

    Fix branch naming inconsistency in "Merge, tada!"

--- problems/merge_tada/verify.js
+++ problems/merge_tada/verify.js
@@ -8,17 +8,18 @@ var exec = require('child_process').exec
 exec('git reflog -10', function(err, stdout, stdrr) {
   var ref = stdout.trim()
   var user = ""
-  
+
   if (ref.match("merge")) console.log("Branch has been merged!")
   else console.log("No merge in the history.")
-  
+
   exec('git config user.username', function(err, stdout, stdrr) {
     user = stdout.trim()
-    
+
     exec('git branch', function(err, stdout, stdrr) {
       branches = stdout.trim()
-      
-      if (branches.match(user)) console.log("Uh oh, branch is still there.")
+      branchName = "add-" + user
+
+      if (branches.match(branchName)) console.log("Uh oh, branch is still there.")
       else return console.log("Branch deleted!")
     })
   })
